1183a3b05255737e905aaa436874292f09b0539c
yfern328 <yfernan2@u.rochester.edu>
2017-06-08 14:50:47 -0400

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,21 @@
-# code here!
+class School
+  attr_reader :roster
+
+  def initialize(name)
+    @name = name
+    @roster = Hash.new {|k,v| k[v] = []}
+  end
+
+  def add_student(student_name, student_grade)
+    @roster[student_grade] << student_name
+  end
+
+  def grade(grade_number)
+    @roster[grade_number]
+  end
+
+  def sort
+    @roster.map {|grade,student_array| [grade,student_array.sort]}.to_h
+  end
+
+end
